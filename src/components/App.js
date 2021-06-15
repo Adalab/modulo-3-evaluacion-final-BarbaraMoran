d42c85a759f47ecda8f7df18b5735e0fdc152cc6
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-15 19:39:39 +0200

    add fetch and clean Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import "../stylesheets/App.scss";
+import { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import "../stylesheets/App.scss";
+import getApiData from "../services/api";
 
 const App = () => {
-  
+  getApiData();
+  console.log(getApiData());
 
- 
+  //const [characters, setCharacters] = useState("");
 
-  return (
-    
-  );
+  return <div></div>;
 };
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+const ENDPOINT = "https://rickandmortyapi.com/api/character";
+
+const getApiData = () => {
+  return fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      const cleanData = data.results.map((character) => {
+        return {
+          id: character.id,
+          name: character.name,
+          picture: character.image,
+          species: character.species,
+          planet: character.location.name,
+          episodesNumber: character.episode.length,
+          status: character.status,
+        };
+      });
+      return cleanData;
+    });
+};
+export default getApiData;
