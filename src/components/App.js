2235ca7e2dab4028cbd69dc13de1b2295bb4d248
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-15 18:04:29 +0200

    add folders and files structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "../stylesheets/App.scss";
+import { Switch, Route } from "react-router-dom";
+
+const App = () => {
+  
+
+ 
+
+  return (
+    
+  );
+};
+
+export default App;
